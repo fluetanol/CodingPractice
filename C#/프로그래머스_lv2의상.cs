3497d1dc6283c3b0a34d207f30d445b248d5fae4
fluetanol <mike415415@naver.com>
2024-04-03 21:41:29 +0900

    2024.0.03 코테문제
    프로그래머스
    LV2 의상 C#

--- C#/프로그래머스_lv2의상.cs
+++ C#/프로그래머스_lv2의상.cs
@@ -1,22 +1,46 @@
 //https://school.programmers.co.kr/learn/courses/30/lessons/42578
 //프로그래머스_lv2의상
-//2024.03.14
-//0625
+//2024.04.03
+//n일 걸림 (시간초과...)
 
-0 1 2 3             4
-01 02 03 12 13 23   6
-012 013 023 123     4
-0123                1
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
+public class 프로그래머스_lv2의상 { 
+    
+    void Comb(int choose, int depth, int idx, int[] vals, int sum, ref int answer){
+        if(choose == depth){
+            for(int i=idx; i<vals.Length; i++){
+                sum *= vals[i];
+                answer+= sum;
+            }
+            return;
+        }
+        else{
+            for(int i=idx; i<vals.Length; i++){
+                int ssum = sum;
+                ssum *= vals[i];
+                answer+= ssum;
+                Comb(choose, depth+1, i+1, vals, ssum, ref answer);    
+            }
+        }
+    }
 
-0 1 2 3 4                               n=0
-01 02 03 04 12 13 14 23 24 34           n=1
-012 013 014 023 024 034 123 124 134 234 n=2
+    public int solution(string[,] clothes) {
+        int answer = 0;
+        Dictionary<string,int> d= new Dictionary<string,int>();
+        for(int i=0; i<clothes.Length/2; i++){
+            string c = clothes[i,1];
+            if(d.ContainsKey(c)) d[c]++;
+            else d[c] = 1;
+        }     
+        int[] vals = d.Values.ToArray();
+        Comb(vals.Length-1, 0, 0, vals, 1, ref answer);
+        return answer;
+    }
+}
 
-0123 0124 0144 0234 0244 0344 1234 1234
-0123
-0124
-
-
-0123 0124 0234 1234                     n=3
-01234
+// 모든 경우의 수를 전부 둘러봐야 하는 문제...
+// 조합 알고리즘 모르면 풀기 어려웠던 문제
+// 나중에 다시 풀어볼 만한 가치가 있음.
