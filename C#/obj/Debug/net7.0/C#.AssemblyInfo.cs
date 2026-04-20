ef6651d94da32f629a44c1276c7b9dc4eba55904
Jung TaeSeung <mike415415@naver.com>
2026-04-20 17:58:21 +0900

    chore

--- C#/obj/Debug/net7.0/C#.AssemblyInfo.cs
+++ C#/obj/Debug/net7.0/C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+610b3d439bc88fd201216e4082fe8ad4eb2c31c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5fcacd3a21d72de09d6202ab8b06a686574d82a")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
